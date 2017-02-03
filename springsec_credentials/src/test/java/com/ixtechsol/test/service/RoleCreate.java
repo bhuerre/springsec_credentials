f4a8437691c732ec8b2746ed4fc4a93fdaad8d53
bhuerre <bhuerre@cent64dev01.internal.ixtechsol.com>
2017-02-02 23:45:41 -0600

    Added to Role Create test

--- springsec_credentials/src/test/java/com/ixtechsol/test/service/RoleCreate.java
+++ springsec_credentials/src/test/java/com/ixtechsol/test/service/RoleCreate.java
@@ -11,23 +11,29 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.ixtechsol.sec.model.Role;
 import com.ixtechsol.sec.service.IRoleService;
 import com.ixtechsol.sec.validation.RoleExistsException;
+import com.ixtechsol.sec.validation.RoleNotFoundException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextDefinition
 public class RoleCreate {
 	Logger logger = LoggerFactory.getLogger(RoleCreate.class);
 	
-	private static final String USER_ROLE = "USER";
+	private static final String USER_ROLE = "USER1";
     
 	@Autowired
 	IRoleService roleService;
 	
 	@Test
-	public void whenCreateRole_thenSuccess() throws RoleExistsException{
+	public void whenCreateRole_thenSuccess() throws RoleExistsException,RoleNotFoundException{
 		logger.info("\n");
 		logger.info("IN whenCreateRole_thenSuccess()");
-		Role role = new Role(USER_ROLE);
+		Role role = roleService.findRoleByName(USER_ROLE);
+		if (role != null ) {
+			roleService.deleteRole(role);
+			logger.info("\tDelete role {}",USER_ROLE);
+		} 
+		role.setName(USER_ROLE);
 		roleService.registerNewRole(role);
-		System.out.println(roleService.findRoleByName(USER_ROLE).equals(role));
+		logger.info("OUT whenCreateRole_thenSuccess()");
 	};
 }
